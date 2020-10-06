874281b0652fcdc2b192bf6e53f355324b15f4b5
Nico Ohnezat <nico@no-12.net>
2020-10-06 21:14:29 +0200

    install packages with role

--- run_role_locally.sh
+++ run_role_locally.sh
@@ -1,12 +1,34 @@
 #!/usr/bin/env bash
-source "venv/bin/activate"
-declare ROLE_DIR
 ROLE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+EXTRA_ARGS=(--skip-tags install_packages)
+
+print_usage() {
+  echo "Usage:"
+  echo "    ${BASH_SOURCE[0]} -h        Display this help message."
+  echo "    ${BASH_SOURCE[0]} -p        Install packages (requires sudo)"
+}
+
+while getopts "hp" opt; do
+  case ${opt} in
+  h) print_usage && exit 0 ;;
+  p) EXTRA_ARGS=(--ask-become-pass) ;;
+  *) print_usage exit 1 ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  EXTRA_ARGS=(--)
+fi
+
+source "venv/bin/activate"
+
 ansible-playbook \
   -v \
   --connection local \
   --inventory localhost, \
   --extra-vars "role_dir=$ROLE_DIR" \
+  "${EXTRA_ARGS[@]}" \
   /dev/stdin <<EOF
 ---
 - hosts: all
