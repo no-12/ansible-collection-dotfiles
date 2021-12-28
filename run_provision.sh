03f89c48901e21273ad2423b9c9b1059106cf1f9
Nico Ohnezat <nico@no-12.net>
2021-12-28 01:18:56 +0100

    unified provision playbook

--- run_provision.sh
+++ run_provision.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+set -eu
+
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 export ANSIBLE_COLLECTIONS_PATHS="$DIR":~/.ansible/collections:/usr/share/ansible/collections
 
 ansible-playbook \
-  -v \
+  -vv \
   --connection local \
-  --inventory "$(hostname)", \
-  no_12.dotfiles.provision_macos
+  --inventory "$1", \
+  no_12.dotfiles.provision
