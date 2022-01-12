444c85c92f7857b3aaed8282fcd15c34e01c5f3b
Nico Ohnezat <nico@no-12.net>
2022-01-12 22:55:48 +0100

    do not ask become pass for macos

--- run_provision.sh
+++ run_provision.sh
@@ -5,9 +5,14 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 export ANSIBLE_COLLECTIONS_PATHS="$DIR":~/.ansible/collections:/usr/share/ansible/collections
 
+ASK_BECOME_PASS=""
+if [ "$(uname)" != "Darwin" ]; then
+  ASK_BECOME_PASS="--ask-become-pass"
+fi
+
 ansible-playbook \
   -vv \
   --connection local \
   --inventory "$1", \
-  --ask-become-pass \
+  $ASK_BECOME_PASS \
   no_12.dotfiles.provision
