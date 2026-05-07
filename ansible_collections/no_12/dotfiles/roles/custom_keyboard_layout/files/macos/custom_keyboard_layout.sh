2c5ed612ad345bb830b5ef69147f0dc565a90513
Nico Ohnezat <nico@no-12.net>
2026-05-07 16:41:10 +0200

    tmux

--- ansible_collections/no_12/dotfiles/roles/custom_keyboard_layout/files/macos/custom_keyboard_layout.sh
+++ ansible_collections/no_12/dotfiles/roles/custom_keyboard_layout/files/macos/custom_keyboard_layout.sh
@@ -5,44 +5,12 @@
 # switch "right option" (0x7000000E6) and "right command" (0x7000000E7)
 # switch "`/~" (0x700000035) and "§/±" (0x700000064)
 
-key_map_pc=$(cat <<-END
-{
-    "UserKeyMapping":[
-        {
-            "HIDKeyboardModifierMappingSrc":0x700000039,
-            "HIDKeyboardModifierMappingDst":0x700000045
-        }
-    ]
-}
-END
-)
-
 key_map_apple=$(cat <<-END
 {
     "UserKeyMapping":[
         {
             "HIDKeyboardModifierMappingSrc":0x700000039,
             "HIDKeyboardModifierMappingDst":0x700000045
-        },
-        {
-            "HIDKeyboardModifierMappingSrc":0x7000000E2,
-            "HIDKeyboardModifierMappingDst":0x7000000E3
-        },
-        {
-            "HIDKeyboardModifierMappingSrc":0x7000000E3,
-            "HIDKeyboardModifierMappingDst":0x7000000E2
-        },
-        {
-            "HIDKeyboardModifierMappingSrc":0x7000000E6,
-            "HIDKeyboardModifierMappingDst":0x7000000E7
-        },
-        {
-            "HIDKeyboardModifierMappingSrc":0x7000000E7,
-            "HIDKeyboardModifierMappingDst":0x7000000E6
-        },
-        {
-            "HIDKeyboardModifierMappingSrc":0x700000035,
-            "HIDKeyboardModifierMappingDst":0x700000064
         },
         {
             "HIDKeyboardModifierMappingSrc":0x700000064,
@@ -53,11 +21,8 @@ key_map_apple=$(cat <<-END
 END
 )
 
-# DasKeyboard
-hidutil property --matching "{\"ProductID\":0x0142}" property --set "$key_map_pc"
+# Keychron Q1 Max
+hidutil property --matching "{\"VendorID\":0x3434}" property --matching "{\"ProductID\":0x0810}" property --set "$key_map_apple"
 
 # Builtin keyboard
-hidutil property --matching "{\"ProductID\":0x0342}" property --set "$key_map_apple"
-
-# Magic keyboard
-hidutil property --matching "{\"ProductID\":0x026c}" property --set "$key_map_apple"
+hidutil property --matching "{\"VendorID\":0x05ac}" property --matching "{\"ProductID\":0x0342}" property --set "$key_map_apple"
--- ansible_collections/no_12/dotfiles/roles/zsh/templates/.zshrc
+++ ansible_collections/no_12/dotfiles/roles/zsh/templates/.zshrc
@@ -1,3 +1,5 @@
+[[ ! $TERM =~ screen ]] && [[ ! $TERM_PROGRAM =~ vscode ]] && exec tmux new -A -s main
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
@@ -250,5 +252,3 @@ hash op &> /dev/null && source <(op completion zsh)
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-
-[[ ! $TERM =~ screen ]] && [[ ! $TERM_PROGRAM =~ vscode ]] && exec tmux new -A -s main
